9e14297c7fd73329fef26196be609ab1d3d43cae
june mawia <mawia9301@gmail.com>
2026-03-27 13:41:49 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# 1. Initialize the API
+app = FastAPI(title="June's Capstone API", description="A beginner-friendly toolkit for FastAPI")
 
+# 2. Create a "Home" endpoint
 @app.get("/")
-def read_root():
-    return {"Hello": "June, your API is working!"}
+def home():
+    return {
+        "Status": "Online",
+        "Message": "Welcome to my FastAPI Toolkit!",
+        "Next_Step": "Go to /docs to test the API visually"
+    }
+
+# 3. Create a "Dynamic" endpoint
+@app.get("/greet/{name}")
+def greet_user(name: str):
+    return {"message": f"Hello {name}, FastAPI is working perfectly!"}
